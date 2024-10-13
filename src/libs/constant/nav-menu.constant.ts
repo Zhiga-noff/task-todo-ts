a0ff74324b57c65d4ebf1edcd798be6b4386dde9
kirill <kzhiganov@zvezdamedia.ru>
2024-10-13 23:51:36 +0300

    Хочу сделать другую ветку для пэйдежес

--- src/libs/constant/nav-menu.constant.ts
+++
@@ -1,5 +0,0 @@
-export const navMenuConstant: { name: 'All' | 'Completed' | 'Active'; href: string }[] = [
-    {name: 'All', href: '/'},
-    {name: 'Active', href: '/active'},
-    {name: 'Completed', href: '/completed'},
-];
